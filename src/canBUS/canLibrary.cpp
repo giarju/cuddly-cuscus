b4dc7f1534e99ba0d27a8ff5841d0754e2533afe
rayleigh18 <sitanggangirfan271000@gmail.com>
2019-12-31 07:59:43 +0700

    Menambahkan SendKpKiKd

--- src/canBUS/canLibrary.cpp
+++ src/canBUS/canLibrary.cpp
@@ -1,19 +1,34 @@
 #include "mbed.h"
 #include "canLibrary.h"
 
+#define ID_DATA_Kp  
+#define ID_DATA_Ki
+#define ID_DATA_Kd
+#define ID_DATA_4
 
-
-void sendDataToSlave(int ID_DATA, float* msgAddres, procType proc){
-    CANMessage msg(ID_DATA, reinterpret_cast<char*>(msgAddres), 4);
+void canSetup(){
+    can.frequency(FREKUENSI_CAN);
+}
+void sendDataToSlave(int ID_DATA, float* _msg_addres, procType proc){   // procedure send data to Slave
+    CANMessage msg(ID_DATA, reinterpret_cast<char*>(_msg_addres), 4);   // proc is procedure
     if (msg.write(msg)){
         proc();
     }
-    wait(WAIT_SEND);
 }
+void doNothing(){
 
-int receiveData(int ID_DATA, int* var){
+}
+void receiveData(int ID_DATA, int* var){                               //procedure receive data
     if (msg.id == ID_DATA){
         *var = *reinterpret_cast<int*>(msg.data);
-        wait(WAIT_CAN);
     }
+}
+
+
+void sendKpKiKd(float Kp, float Ki, float Kd){
+    sendDataToSlave(ID_DATA_Kp, Kp, doNothing);
+    wait(WAIT_SEND);
+    sendDataToSlave(ID_DATA_Ki, Ki, doNothing);
+    wait(WAIT_SEND);
+    sendDataToSlave(ID_DATA_Kd, Kd, doNothing);
 }
--- src/canBUS/canLibrary.h
+++ src/canBUS/canLibrary.h
@@ -4,11 +4,17 @@
 #include "mbed.h"
 #define WAIT_SEND        0.005
 #define WAIT_CAN         0.05
+#define FREKUENSI_CAN  	200000
 
 typedef void (*procType) (void);
 
 void sendDataToSlave(int ID_DATA, float* msgAddres, procType proc);
 
+void canSetup();
+
 int receiveData(int ID_DATA, int* var);
 
+void sendKpKiKd(float Kp, float Ki, float Kd);
+
+void sendSetPoint()
 #endif
